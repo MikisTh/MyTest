cb4187d82a02f75d452cc93040bb742acf706f09
aluno <aluno@embarquedigital.portodigital.local>
2024-02-20 14:19:56 -0300

    commit finish

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { PrismaClient } from '@prisma/client'
+import express from 'express'
+
+const prisma = new PrismaClient()
+const app = express()
+
+app.use(express.json())
+
+// ... your REST API routes will go here
+
+app.listen(3000, () =>
+  console.log('REST API server ready at: http://localhost:3000'),
+)
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
